fa6d777a034d73720ac7cb9ec6b0f03b61d4681d
user@teacher <user@teacher>
2009-08-19 07:07:24 -0500

    minor changes to teacher

--- teacher/teacher.py
+++ teacher/teacher.py
@@ -67,6 +67,8 @@ def main():
         if prev_symlink != symbolic_link:
             print "Symbolic link set to", symbolic_link
             print ""
+        else:
+            print "Symbolic link unchanged"
         
 def update_symlink(link_name, cur_link, current_files, bad_files):
     if cur_link in bad_files or cur_link == None:
