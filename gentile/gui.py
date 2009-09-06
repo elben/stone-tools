0a5512105575eaf408c2709487938d9c32a45094
Elben Shira <elbenshira@gmail.com>
2009-09-06 12:11:51 -0500

    mplayer starts at last good position

--- gentile/gui.py
+++ gentile/gui.py
@@ -44,6 +44,8 @@ def init_curses():
     if curses.has_colors():
         curses.start_color()
         curses.use_default_colors()
+
+        # color selection
         curses.init_pair(1, curses.COLOR_YELLOW, -1)
         curses.init_pair(2, curses.COLOR_RED, -1)
         curses.init_pair(3, curses.COLOR_GREEN, -1)
@@ -51,6 +53,14 @@ def init_curses():
     gui.s.nodelay(1)    # stop getch() from blocking
     gui.s.keypad(1)
 
+def addstr(r, c, s, attr=None):
+    # attr is not only color, but to make things easier, don't print
+    # attr styles if we don't have colors
+    if curses.has_colors() and attr is not None:
+        gui.s.addstr(r, c, s, attr)
+    else:
+        gui.s.addstr(r, c, s)
+        
 def restore_screen():
     curses.nocbreak()
     curses.echo()
@@ -88,24 +98,24 @@ def progress_bar(percent=0, row=0, col=0, color=0, width=70):
     gui.s.addch(row+1, col+width+1, curses.ACS_VLINE)
 
     if curses.has_colors():
-        gui.s.addstr(row+1, col+1, s, curses.color_pair(color))
-        gui.s.addstr(row+1, col + (width/2)-1, str(percent)+"%",
+        addstr(row+1, col+1, s, curses.color_pair(color))
+        addstr(row+1, col + (width/2)-1, str(percent)+"%",
                 curses.color_pair(color))
     else:
-        gui.s.addstr(row+1, col+1, s)
-        gui.s.addstr(row+1, col + (width/2)-2, str(percent)+" %")
+        addstr(row+1, col+1, s)
+        addstr(row+1, col + (width/2)-2, str(percent)+" %")
 
 def draw_title(title="Gentile Monitor", border=True):
     gui.s.border() # draw sweet border
     start_col = gui.w/2 - len(title)/2
-    gui.s.addstr(1, start_col, title)
+    addstr(1, start_col, title)
     if border:
         gui.s.hline(2, 1, curses.ACS_HLINE, gui.w-2)
         gui.s.addch(2, 0, curses.ACS_LTEE)
         gui.s.addch(2, gui.w-1, curses.ACS_RTEE)
 
 def draw_status(status="press 'h' for help"):
-    gui.s.addstr(gui.h-2, 1, status.center(gui.w-2))
+    addstr(gui.h-2, 1, status.center(gui.w-2))
 
 def draw_help(row=3, col=48):
     help = [
@@ -131,12 +141,15 @@ def draw_help(row=3, col=48):
     
     for h in help:
         if curses.has_colors():
-            gui.s.addstr(row, col, h, curses.color_pair(4))
+            addstr(row, col, h, curses.color_pair(4))
         else:
-            gui.s.addstr(row, col, h)
+            addstr(row, col, h)
         row += 1
 
 def find_video_ptrs():
+    """
+    Returns list of video pointer files on Paul.
+    """
     delay_time = 1  # seconds
     if time.time() - gui.ptr_delay >= delay_time:
         ptr_re = re.compile('"[^"]*\.' + FILE_EXT + '\"')
@@ -169,14 +182,14 @@ def draw_selector(selection, selected_row=0):
     row_disp = 3
     for v in range(len(selection)):
         if selected_row%len(selection) == v:
-            gui.s.addstr(row_disp, 1, str(selection[v]), curses.A_STANDOUT)
+            addstr(row_disp, 1, str(selection[v]), curses.A_STANDOUT)
         else:
-            gui.s.addstr(row_disp, 1, str(selection[v]))
+            addstr(row_disp, 1, str(selection[v]))
         row_disp += 1
 
 def mplayer_status(file, seek=1024):
     """Returns string in HH:MM:SS format."""
-    mp_status_re = re.compile(" +V: +[0-9]+\.[0-9]")
+    mp_status_re = re.compile("A: +[0-9]+\.[0-9] V")
       
     with open(file, "r") as f:
         # find pos to seek to; make sure it is legal pos
@@ -186,9 +199,9 @@ def mplayer_status(file, seek=1024):
         found = mp_status_re.findall(contents)
         if len(found) >= 1:
             last = found[-1]
-            last = last.strip(" V:")
+            last = last.strip("AV: ")
             return int(float(last))
-        return 0
+        raise Exception("Did not find playback status.")
 
 def secs2str(secs):
     secs = int(secs)
@@ -221,8 +234,8 @@ def video_select():
         elif c == 10:
             # keypress ENTER
             break
-        gui.s.addstr(20, 1, str(c))
-        time.sleep(.001)
+        addstr(20, 1, str(c))
+        time.sleep(.02)
     selected_ptr %= len(gui.ptr_files) 
     return selected_ptr
 
@@ -246,7 +259,7 @@ def main():
         # Wget setup
         # get the download location from the pointer file we were passed
         while not ptr_wget.finished():
-            gui.s.addstr(20, 30, "Downloading...")
+            addstr(20, 30, "Downloading...")
             time.sleep(.1)
         try:
             with open(ptr_file, "r") as file:
@@ -278,73 +291,77 @@ def main():
     mplayer_size = secs2bytes(15)   # 15 second buffer
     mplayer_start = False           # wait for user to start mplayer
     a_v_diff = 0
+    time_playback = 0               # secs into video (from mplayer status)
     display_help = False
-    while True:
+    continue_loop = True
+    while continue_loop:
         gui.s.erase()
         
-        c = gui.s.getch()
-        gui.s.addstr(21, 4, str(c))    # display key vals for debug
-        
-        # handle input
-        if c == ord('q'):
-            wget.terminate()
-            if mplayer is not None:
-                mplayer.terminate()
-            break
-        elif c == ord('d'):
-            download_file = not download_file 
-        elif c == ord('m'):
-            mplayer_start = not mplayer_start
-        elif c == ord('h'):
-            display_help = not display_help
-        elif c == 32:   # space bar
-            if mplayer is not None:
-                mplayer.stdin.write('p')    # pause
-        elif c == ord('o'):
-            if mplayer is not None:
-                mplayer.stdin.write('o')    # toggle OSD display
-        elif c == 260:  # left arrow
-            if mplayer is not None:
-                mplayer.stdin.write('h')    # short seek left
-        elif c == 261:  # right arrow
-            if mplayer is not None:
-                mplayer.stdin.write('j')    # short seek right
-        elif c == 44:   # ,
-            if mplayer is not None:
-                mplayer.stdin.write('g')    # medium seek left
-        elif c == 46:   # .
-            if mplayer is not None:
-                mplayer.stdin.write('k')    # medium seek right
-        elif c == 91:   # [
-            if mplayer is not None:
-                mplayer.stdin.write('f')    # long seek left
-        elif c == 93:   # ]
-            if mplayer is not None:
-                mplayer.stdin.write('l')    # long seek right
-        elif c == 45:   # -
-            if mplayer is not None:
-                mplayer.stdin.write('z')    # short sound sync left
-                a_v_diff -= 5
-        elif c == 61:   # =
-            if mplayer is not None:
-                mplayer.stdin.write('x')    # short sound sync right
-                a_v_diff += 5
-        elif c == 57:   # 9
-            if mplayer is not None:
-                mplayer.stdin.write('a')    # medium sound sync left
-                a_v_diff -= 50
-        elif c == 48:   # 0
-            if mplayer is not None:
-                mplayer.stdin.write('s')    # medium sound sync right
-                a_v_diff += 50
-        elif c == 55:   # 7
-            if mplayer is not None:
-                mplayer.stdin.write('q')    # long sound sync right
-                a_v_diff -= 500
-        elif c == 56:   # 8
-            if mplayer is not None:
-                mplayer.stdin.write('w')    # long sound sync right
-                a_v_diff += 500
+        c = 0
+        while c != -1:
+            c = gui.s.getch()
+            addstr(21, 4, str(c))    # display key vals for debug
+            
+            # handle input
+            if c == ord('q'):
+                wget.terminate()
+                if mplayer is not None:
+                    mplayer.terminate()
+                continue_loop = False
+            elif c == ord('d'):
+                download_file = not download_file 
+            elif c == ord('m'):
+                mplayer_start = not mplayer_start
+            elif c == ord('h'):
+                display_help = not display_help
+            elif c == 32:   # space bar
+                if mplayer is not None:
+                    mplayer.stdin.write('p')    # pause
+            elif c == ord('o'):
+                if mplayer is not None:
+                    mplayer.stdin.write('o')    # toggle OSD display
+            elif c == 260:  # left arrow
+                if mplayer is not None:
+                    mplayer.stdin.write('h')    # short seek left
+            elif c == 261:  # right arrow
+                if mplayer is not None:
+                    mplayer.stdin.write('j')    # short seek right
+            elif c == 44:   # ,
+                if mplayer is not None:
+                    mplayer.stdin.write('g')    # medium seek left
+            elif c == 46:   # .
+                if mplayer is not None:
+                    mplayer.stdin.write('k')    # medium seek right
+            elif c == 91:   # [
+                if mplayer is not None:
+                    mplayer.stdin.write('f')    # long seek left
+            elif c == 93:   # ]
+                if mplayer is not None:
+                    mplayer.stdin.write('l')    # long seek right
+            elif c == 45:   # -
+                if mplayer is not None:
+                    mplayer.stdin.write('z')    # short sound sync left
+                    a_v_diff -= 5
+            elif c == 61:   # =
+                if mplayer is not None:
+                    mplayer.stdin.write('x')    # short sound sync right
+                    a_v_diff += 5
+            elif c == 57:   # 9
+                if mplayer is not None:
+                    mplayer.stdin.write('a')    # medium sound sync left
+                    a_v_diff -= 50
+            elif c == 48:   # 0
+                if mplayer is not None:
+                    mplayer.stdin.write('s')    # medium sound sync right
+                    a_v_diff += 50
+            elif c == 55:   # 7
+                if mplayer is not None:
+                    mplayer.stdin.write('q')    # long sound sync right
+                    a_v_diff -= 500
+            elif c == 56:   # 8
+                if mplayer is not None:
+                    mplayer.stdin.write('w')    # long sound sync right
+                    a_v_diff += 500
         
         # wget process
         if download_file:
@@ -357,9 +374,11 @@ def main():
         if ( mplayer_start and
              ( mplayer == None or mplayer.poll() != None ) and
              wget.size_local() > mplayer_size ):
-            mp_args = ["mplayer", "-osdlevel", str(OSDLEVEL), "-mc",
-                    str(MAX_AV_CORRECTION), "-framedrop", "-delay",
-                    str(DEFAULT_AV_DELAY), LOCAL_FILE]
+            mp_args = ["mplayer", "-osdlevel", str(OSDLEVEL),
+                    "-mc", str(MAX_AV_CORRECTION), "-framedrop",
+                    "-delay", str(DEFAULT_AV_DELAY),
+                    "-ss", str(time_playback),
+                    LOCAL_FILE]
             mplayer = sp.Popen(mp_args, stdout=mplayer_stdout_file,
                                stderr=mplayer_stderr_file, stdin=sp.PIPE)
             a_v_diff = int(DEFAULT_AV_DELAY * 1000)
@@ -371,57 +390,73 @@ def main():
         # download and playback statistics
         time_local = bytes2secs(wget.size_local())
         time_remote = bytes2secs(wget.size_remote())
-        time_playback = mplayer_status(MPLAYER_STDOUT_FILE)
+        try:
+            new_time = mplayer_status(MPLAYER_STDOUT_FILE)
+            time_playback = new_time
+        except:
+            # could not extract status from mplayer's stdout;
+            # keep old value
+            pass
         
         draw_title()
         
         # display download progress bar
-        bar_color = 3 # green
-        if not wget.alive():
-            bar_color = 1 # yellow
-        if not download_file:
-            bar_color = 2 # red
-        progress_bar(wget.progress(), 6, 4, bar_color)
+        progress_bar(wget.progress(), 6, 4)
         
         # display download stats
-        gui.s.addstr(4, 2, 'Download Status', curses.A_UNDERLINE)
-        gui.s.addstr(4, 18, '(' + wget.url() + ')')
-        gui.s.addstr(9, 4, "Size: " +
+        addstr(4, 2, 'Download Status', curses.A_UNDERLINE)
+        addstr(4, 18, '(wget')
+
+        if not download_file:
+            addstr(4, 24, 'off', curses.color_pair(2))
+            addstr(4, 27, ')')
+        #elif not wget.alive():
+        #    addstr(4, 24, 'pause', curses.color_pair(1))
+        #    addstr(4, 29, ')')
+        else:
+            addstr(4, 24, 'on', curses.color_pair(3))
+            addstr(4, 26, ')')
+        addstr(4, 29, '(' + wget.url().lstrip('http://') + ')')
+
+        addstr(9, 4, "Size: " +
             "{0:0.1f}".format(float(wget.size_local())/1024/1024) +
             " of " +
             "{0:0.1f}".format(float(wget.size_remote())/1024/1024) +
             " MB")
-        gui.s.addstr(10, 4, "Time: " + secs2str(time_local) + " of " +
+        addstr(10, 4, "Time: " + secs2str(time_local) + " of " +
                      secs2str(time_remote))
-        gui.s.addstr(11, 4, "Rate: " + str(int(wget.rate())/1024/1024) +
+        addstr(11, 4, "Rate: " + str(int(wget.rate())/1024/1024) +
                      " MB/s")
-        #gui.s.addstr(12, 4, "Time until catch-up: unimplemented")
+        #addstr(12, 4, "Time until catch-up: unimplemented")
 
         # display playback status
-        gui.s.addstr(13, 2, 'Playback Status', curses.A_UNDERLINE)
-        gui.s.addstr(18, 4, "Time: " + secs2str(time_playback)
+        addstr(13, 2, 'Playback Status', curses.A_UNDERLINE)
+        addstr(13, 18, '(mplayer')
+        if mplayer is not None:
+            addstr(13, 27, 'on', curses.color_pair(3))
+            addstr(13, 29, ')')
+        else:
+            addstr(13, 27, 'off', curses.color_pair(2))
+            addstr(13, 30, ')')
+
+        addstr(18, 4, "Time: " + secs2str(time_playback)
                 + " of " + secs2str(time_local))
-        gui.s.addstr(19, 4, "Time Remaining: " +
+        addstr(19, 4, "Time Remaining: " +
                 secs2str(time_local-time_playback))
-        gui.s.addstr(20, 4, "A-V: " + str(a_v_diff) + " ms")
+        addstr(20, 4, "A-V: " + str(a_v_diff) + " ms")
 
         # display playback progress bar
         try:
             playback_progress = time_playback / time_local
         except:
             playback_progress = 0
-        bar_color = 3   # green
-        if mplayer_start and mplayer is None:
-            bar_color = 1   # yellow
-        if not mplayer_start:
-            bar_color = 2   # red
-        progress_bar(playback_progress, 15, 4, bar_color)
+        progress_bar(playback_progress, 15, 4)
 
         draw_status()
         if display_help:
             draw_help()
         gui.s.refresh()
-        time.sleep(0.05) # to kill spinning
+        time.sleep(0.1) # to kill spinning
     
     mplayer_stdout_file.close()
     mplayer_stderr_file.close()
