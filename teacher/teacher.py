5c72e724b3fda11aa8b99cad89fd2bca70c7b03f
user@teacher <user@teacher>
2009-10-03 12:04:53 -0500

    refactored teacher, still works (at least, it does now)

--- teacher/teacher.py
+++ teacher/teacher.py
@@ -18,8 +18,8 @@ VIDEO_PREFIX = configs.get("teacher", "video_prefix")
 
 def main():
     # initialize 
-    current = []
-    previous = []
+    current_files = []
+    previous_files = []
     bad_files = []
     
     prev_symlink = None
@@ -28,33 +28,32 @@ def main():
     print "Starting program..."
     while True:
         # done at beginning to prevent spamming messages via 'continue'
-        sleep_time = 2
+        sleep_time = 2.5
         time.sleep(sleep_time)
         
         # keep previous sizes
-        previous = []
-        previous.extend(current)
+        previous_files = []
+        previous_files.extend(current_files)
         
         # get files and their sizes from the directory
-        current = get_video_files(VIDEO_DIR, VIDEO_PREFIX)
+        current_files = get_video_files(VIDEO_DIR, VIDEO_PREFIX)
         
-        if current == []:
+        if current_files == []:
             print "Waiting for video files..."
         
         # find bad files
-        for cur_name, cur_size in current:
-            for prev_name, prev_size in previous:
+        for cur_name, cur_size in current_files:
+            for prev_name, prev_size in previous_files:
                 # if files have the same name and the size has not grown
                 # and that file is not already in bad_files
-                if ( cur_name == prev_name and
-                     cur_size <= prev_size ):
-                    
+                if ( cur_name == prev_name and cur_size <= prev_size ):
                     # don't add new recordings to bad_files
                     if cur_size <= 0.5 * prev_size:
                         # if it has shrunk drastically, it is no longer bad
                         print
                         print "New sermon found, removing from bad files"
                         
+                        # remove it whether it's in bad_files or not
                         try:
                             bad_files.remove( cur_name )
                         except:
@@ -64,15 +63,15 @@ def main():
                         if not cur_name in bad_files:
                             bad_files.append( cur_name )
                             
-                            print "Added bad file" + cur_name
+                            print "Added bad file", "'" + cur_name + "'"
                     
-        if len(bad_files) == len(current) and len(current) != 0:
+        if len(bad_files) == len(current_files) and len(current_files) != 0:
             print "No good files found!"
             
         # update symlink
         link_name = VIDEO_DIR + "sermon_symlink"
         prev_symlink = symbolic_link
-        symbolic_link = update_symlink(link_name, symbolic_link, current,
+        symbolic_link = update_symlink(link_name, symbolic_link, current_files,
                                        bad_files)
         
         # print symlink changes if it changed from the previous iteration
@@ -81,17 +80,23 @@ def main():
             print ""
         else:
             print "Symbolic link is", "'" + str(symbolic_link) + "'"
-        
+    
 def update_symlink(link_name, cur_link, current_files, bad_files):
     """
-    
+    Update the current symlink to point to a knwn good file
     """
+    # return this, stays the same if cur_link is not bad
+    new_link = cur_link
+    
     # as long as the symlink is to a bad file or does not exist
     if cur_link in bad_files or cur_link == None:
         # look through all the files and compare to the ones in bad_files
         for file_name, file_size in current_files:
             # set link to a file that is not bad
             if file_name not in bad_files:
+                # we will return this
+                new_link = file_name
+
                 # remove old link if it exists
                 rm(link_name)
                 
@@ -100,7 +105,7 @@ def update_symlink(link_name, cur_link, current_files, bad_files):
                 
                 break
     
-    return cur_link
+    return new_link
 
 def rm(file):
     """
