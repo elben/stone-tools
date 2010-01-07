59b9d5eaf0f6bd3674716f59b9566fcee02c6929
Jason Bradshaw <jasontbradshaw@gmail.com>
2010-01-07 17:48:50 -0600

    merge

--- tests/downloader_test.py
+++ tests/downloader_test.py
@@ -55,106 +55,105 @@ class DownloaderTest(unittest.TestCase):
         self.assert_( type(rate) is type(0.0) )
         
         self.d2.stop()
-        exit()
 
-#     def test_download1(self):
-#         self.d1.start()
+    def test_download1(self):
+        self.d1.start()
         
-#         while self.d1.get_progress() < 1.0:
-#             time.sleep(0.1)
+        while self.d1.get_progress() < 1.0:
+            time.sleep(0.1)
         
-#         self.d1.stop()
+        self.d1.stop()
             
-#         str  = "1 this is a test download\n"
-#         str += "2 many lines are here\n"
-#         str += "3 well, mostly three\n"
+        str  = "1 this is a test download\n"
+        str += "2 many lines are here\n"
+        str += "3 well, mostly three\n"
         
-#         data = ""
-#         with open(self.d1.get_local_path()) as f:
-#             data = f.read()
+        data = ""
+        with open(self.d1.get_local_path()) as f:
+            data = f.read()
         
-#         self.assert_(data == str)
+        self.assert_(data == str)
 
-#     def test_download2(self):
-#         self.d2.start()
+    def test_download2(self):
+        self.d2.start()
         
-#         while self.d2.get_progress() < 1.0:
-#             time.sleep(0.1)
+        while self.d2.get_progress() < 1.0:
+            time.sleep(0.1)
         
-#         self.d2.stop()
+        self.d2.stop()
         
-#         str  = "1234567890"
+        str  = "1234567890"
         
-#         data = ""
-#         with open(self.d2.get_local_path()) as f:
-#             data = f.read()
+        data = ""
+        with open(self.d2.get_local_path()) as f:
+            data = f.read()
             
-#         self.assert_(data == str)
+        self.assert_(data == str)
 
-#     def test_download3(self):
-#         self.d3.start()
+    def test_download3(self):
+        self.d3.start()
         
-#         while self.d3.get_progress() < 1.0:
-#             time.sleep(0.1)
+        while self.d3.get_progress() < 1.0:
+            time.sleep(0.1)
         
-#         self.d3.stop()
+        self.d3.stop()
             
-#         data = ""
-#         with open(self.d3.get_local_path()) as f:
-#             data = f.read()
+        data = ""
+        with open(self.d3.get_local_path()) as f:
+            data = f.read()
             
-#             self.assert_( len(data) == 2490041 )
+            self.assert_( len(data) == 2490041 )
+    
+    def test_download4(self):
+        self.d4.start()
+        
+        while self.d4.get_progress() < 1.0:
+            time.sleep(0.1)
+        
+        self.d4.stop()
+            
+        data = None
+        with open(self.d4.get_local_path()) as f:
+            data = f.read()
+            
+        self.assert_( data == "" )
+    
+    def test_download_resume(self):
+        self.d3.start()
+        time.sleep(0.1)
+        self.d3.stop()
+        time.sleep(0.1)
+        self.d3.start()
+        
+        while self.d3.get_progress() < 1.0:
+            time.sleep(0.1)
+        
+        self.d3.stop()
+            
+        data = ""
+        with open(self.d3.get_local_path()) as f:
+            data = f.read()
+            
+        self.assert_( len(data) == 2490041 )
 
-#     def test_download4(self):
-#         self.d4.start()
+    def test_download_comm(self):
+        # try to freak out the thread communication
+        for i in xrange(1000):
+            self.d3.start()
+            self.d3.stop()
+            
+        self.d3.start()
         
-#         while self.d4.get_progress() < 1.0:
-#             time.sleep(0.1)
+        while self.d3.get_progress() < 1.0:
+            time.sleep(0.1)
         
-#         self.d4.stop()
+        self.d3.stop()
             
-#         data = None
-#         with open(self.d4.get_local_path()) as f:
-#             data = f.read()
+        data = ""
+        with open(self.d3.get_local_path()) as f:
+            data = f.read()
             
-#         self.assert_( data == "" )
-
-#     def test_download_resume(self):
-#         self.d3.start()
-#         time.sleep(0.1)
-#         self.d3.stop()
-#         time.sleep(0.1)
-#         self.d3.start()
-        
-#         while self.d3.get_progress() < 1.0:
-#             time.sleep(0.1)
-        
-#         self.d3.stop()
-            
-#         data = ""
-#         with open(self.d3.get_local_path()) as f:
-#             data = f.read()
-            
-#         self.assert_( len(data) == 2490041 )
-
-#     def test_download_comm(self):
-#         # try to freak out the thread communication
-#         for i in xrange(1000):
-#             self.d3.start()
-#             self.d3.stop()
-            
-#         self.d3.start()
-        
-#         while self.d3.get_progress() < 1.0:
-#             time.sleep(0.1)
-        
-#         self.d3.stop()
-            
-#         data = ""
-#         with open(self.d3.get_local_path()) as f:
-#             data = f.read()
-            
-#         self.assert_( len(data) == 2490041 )
+        self.assert_( len(data) == 2490041 )
 
 if __name__ == '__main__':
     unittest.main()
